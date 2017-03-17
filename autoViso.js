0a0d16f60b3e3bdcb70c91a7b2af97c2d7d9d1cc
ingolfsson <daniel.ingolfsson@gmail.com>
2017-03-17 17:24:37 +0000

    Viso skraning

---
+++ autoViso.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     INSTANT VISO
+// @match  *://www.nord.is/*
+// @author  Ingolfsson
+// @version 1337
+// ==/UserScript==
+
+(document.getElementsByClassName("register-viso")[0]).click();
